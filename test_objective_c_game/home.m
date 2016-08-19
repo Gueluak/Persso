11de9b18a1cfbd668f8eb5401493b6ab4525dd07
Guelual <hmarot@student.42.fr>
2016-08-19 11:20:11 +0200

    Salut ma poule

---
+++ test_objective_c_game/home.m
@@ -0,0 +1,13 @@
+#importe <Fundation/fundation.h>
+
+@implementation objet
+
+ - (BOOL) defineHitBoxWhithCoordonateT_x: (int)t_x t_y:(int)t_y
+ {
+ 	if ([form isEqualToString:@"square"])
+ 		return (t_x <= x + size_x && t_x >= x - size_x && t_y < y + size_y && t_y > y - size_y);
+ 	else if ([form isEqualToString:@"circle"])
+ 	{
+
+ 	}
+ }
